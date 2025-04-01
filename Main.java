487625429f3db25182f39816b0ae406e8edef7a3
ima <mabruns0923@gmail.com>
2025-04-01 12:34:39 -0400

    Got the trucks done (mostly...i think) all I need to do is get the drones working as well then print stats

--- Main.java
+++ Main.java
@@ -8,5 +8,108 @@ public class Main{
         //variables 
         File schedule = new File("train_schedule.txt");
         Scanner scnr = new Scanner(schedule);
+        int truckNums = 0;
+        int truckStopped =0;
+        int timer = 0;
+        int packages = 0;
+        int trainsCrossing = 0;
+        boolean trainpassing = false;
+        boolean trucksDone = false;
+        ArrayList<Truck> compTrucks = new ArrayList<>();
+        ArrayList<Truck> stoppedTrucks = new ArrayList<>();
+        // Creates train schedule
+        ArrayList<Integer> trainSch = TrainSchedule(scnr);
+
+        //main loop
+        while(packages < 1500 || trucksDone){
+
+            // checks if the timer is divisible by 15
+            if(timer % 15 == 0 && packages < 1500){
+                // makes new truck
+                compTrucks.add(new Truck(truckNums, timer));
+                truckNums++;
+                packages += 10;
+            }
+
+            // checks each truck to see when they pass the crossing
+            for(Truck a : compTrucks){
+                if(timer == a.getPassing()){
+                    //checks if the train is passing
+                    if(trainpassing){
+                        a.setStopped(timer);
+                        stoppedTrucks.add(a);
+                    }
+                    else{
+                        a.Passed(timer);
+                    }
+                }
+            }
+
+            // starts the stopped trucks back up
+            if(!trainpassing && stoppedTrucks.size() != 0){
+                stoppedTrucks.remove(0).setUnStopped(timer);
+            }
+
+            for(Truck a : compTrucks){
+                if(a.getTimeDone() == timer){
+                    a.setFinished();
+                }
+            }
+
+            //checks timmer compaired to the next time on the trainSch
+            if(trainsCrossing < 24){
+                if(timer == trainSch.get(trainsCrossing)){
+                    if(trainpassing){
+                        trainpassing = false;
+                        System.out.println(timer + ": TRAIN leaves crossing");
+                    }
+                    else {
+                        trainpassing = true;
+                        System.out.println(timer + ": TRAIN arrives at crossing");
+                    }
+                    trainsCrossing++;
+                }
+            }
+
+            // checks if all trucks are done and if breaks out of the loop
+            for(Truck a : compTrucks){
+                if(!a.getFinished()){
+                    break;
+                }
+                else{
+                    trucksDone = true;
+                }
+            }
+
+
+            //timer up one min
+            timer++;
+        }
+
+    }
+
+    // Creates a Schedule of all the times the train comes and goes
+    public static ArrayList<Integer> TrainSchedule(Scanner scnr){
+        
+        // Variables
+        ArrayList<Integer> trainSch = new ArrayList<>();
+        int pre = 0;
+        int count = 0;
+
+        //grabs every integer from the document
+        while(scnr.hasNextInt()){
+            if(count % 2 == 0){
+                pre = scnr.nextInt();
+                trainSch.add(pre);
+                count++;
+            }
+            else {
+                pre = pre + scnr.nextInt();
+                trainSch.add(pre);
+                count++;
+            }
+        }
+
+        return trainSch;
     }
 }
--- Truck.java
+++ Truck.java
@@ -1,3 +1,73 @@
 public class Truck {
+
+    // Variables
+    private boolean started = false;
+    private boolean stopped = false;
+    private boolean finished = false;
+    private int truckNum;
+    private int timeStart = 0;
+    private int timeDone = 0;
+    private int passCrossing = 0;
+    private int trainStart = 0;
+
+    public Truck(){
+        //does nothing
+    }
+
+    // makes the truck
+    public Truck(int num, int start){
+        started = true;
+        truckNum = num;
+        timeStart = start;
+        passCrossing = start + (3000/30);
+        timeDone = start + (30000/30);
+        System.out.println("TRUCK #" + truckNum + " begins journey");
+    }
     
+    // sets the stop
+    public void setStopped(int timer){
+        stopped = true;
+        trainStart = timer;
+        System.out.println(timer + ": TRUCK #" + truckNum + " waits at crossing");
+            
+    }
+
+    // takes in when the truck is moving again
+    public void setUnStopped(int timer){
+        stopped = false;
+        timeDone = timeDone + timer - trainStart;
+        System.out.println(timer + ": TRUCK #" + truckNum + " crosses crossing");
+    }
+
+    //sets finished
+    public void setFinished(){
+        finished = true;
+        started = false;
+        System.out.println(timeDone + ": TRUCK #" + truckNum + " completes journey");
+    }
+
+    public boolean getFinished(){
+        return finished;
+    }
+
+    public int getTimeDone(){
+        return timeDone;
+    }
+
+    public int getPassing(){
+        return passCrossing;
+    }
+
+    public boolean getStopped(){
+        return stopped;
+    }
+
+    public void Passed(int timer){
+        System.out.println(timer + ": TRUCK #" + truckNum + " crosses crossing");
+    }
+
+    // gives the total time the truck took to delievery the package
+    public int totalTime(){
+        return timeDone;
+    }
 }
